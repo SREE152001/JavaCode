171ae14b2ce75ac8afa377fc4c412695bda2e4a9
Sreedharshan <sakhavusree@gmail.com>
2022-08-15 09:36:09 +0530

    Create withoutPow.java
    code for find without math.pow() method

---
+++ withoutPow.java
@@ -0,0 +1,32 @@
+public class power {
+    public static void main(String[] vvhd)
+    {
+        /*int basenumber = 2, exponent = 3;
+  long temp = 1;
+
+  for (;exponent != 0; --exponent) {
+   temp *= basenumber;
+  }
+
+  System.out.println("Result: " + temp);*/
+        
+        
+        
+        
+        int basenumber = 2, exponent = 3;
+  long temp = 1;
+
+  while (exponent != 0) {
+   temp *= basenumber;
+   --exponent;
+  }
+
+  System.out.println("Result: " + temp);
+        
+        
+        
+        
+          
+    }
+    
+}
