1a81400138f14573b07d7e742b608bf12ea0fd05
Sreedharshan <sakhavusree@gmail.com>
2022-08-15 15:25:40 +0530

    Create anagram.java
    anagram

---
+++ anagram.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class anagram {
+    
+    
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        boolean R=false;
+       a=a.toLowerCase();
+       b=b.toLowerCase();
+       char[] inp=a.toCharArray();
+       Arrays.sort(inp);
+       char[] inp2=b.toCharArray();
+       Arrays.sort(inp2);
+       String c = new String (inp);
+      String d = new String (inp2);
+       if(c.equals(d))
+       {
+           R=true;
+       }
+       return R;
+    }
+
+    
+    
+     public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret==true) ? "Anagrams" : "Not Anagrams" );
+    }
+}
+    
